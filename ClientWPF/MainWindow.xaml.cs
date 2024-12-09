10c6f4eaf0c907903b3917878a057073faefd864
pokapachka <pachkaosokin@yandex.ru>
2024-12-09 18:11:20 +0500

    разработана страница авторизации и прописан ее код. Осокин

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,39 @@ namespace ClientWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public IPAddress IpAddress;
+        public int Port;
+        public int Id = -1;
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPages(new Pages.Autorization());
+        }
+        public Socket ConnectToServer()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IpAddress, Port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    return socket;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"{ex.Message}", "Ошибка");
+                return null;
+            }
+        }
+
+        public void OpenPages(Page page)
+        {
+            frame.Navigate(page);
         }
     }
 }
--- ClientWPF/Pages/Autorization.xaml.cs
+++ ClientWPF/Pages/Autorization.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using Common;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace ClientWPF.Pages
 {
@@ -21,6 +26,65 @@ namespace ClientWPF.Pages
         public Autorization()
         {
             InitializeComponent();
+        }
+
+        private void ClickConnection(object sender, RoutedEventArgs e)
+        {
+            if (!IPAddress.TryParse(IP.Text, out MainWindow.mainWindow.IpAddress))
+            {
+                MessageBox.Show("Указан не правильный ip адрес", "Ошибка");
+                return;
+            }
+
+
+            if (!int.TryParse(Port.Text, out MainWindow.mainWindow.Port))
+            {
+                MessageBox.Show("Указан не правильный port адрес", "Ошибка");
+                return;
+            }
+
+            var socket = MainWindow.mainWindow.ConnectToServer();
+
+            if (socket == null || !ConnectToServer(socket, Login.Text, Password.Text))
+            {
+                MessageBox.Show("Не удалось подключиться", "Ошибка");
+            }
+            else
+            {
+                MainWindow.mainWindow.OpenPages(new Pages.Main());
+            }
+        }
+        public bool ConnectToServer(Socket socket, string login, string password)
+        {
+            try
+            {
+                string command = $"connect {login} {password}";
+                var viewModelSend = new ViewModelSend(command, -1);
+                byte[] messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelSend));
+                socket.Send(messageBytes);
+                byte[] buffer = new byte[1024];
+                int bytesReceived = socket.Receive(buffer);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                ViewModelMessage viewModelMessage = JsonConvert.DeserializeObject<ViewModelMessage>(response);
+                socket.Close();
+                if (viewModelMessage.Command == "autorization")
+                {
+                    int userId = int.Parse(viewModelMessage.Data);
+                    MainWindow.mainWindow.Id = userId;
+                    MessageBox.Show($"Успешная авторизация. Ваш ID: {userId}", "Успех");
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка авторизации: {viewModelMessage.Data}", "Ошибка");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при подключении: {ex.Message}", "Ошибка");
+                return false;
+            }
         }
     }
 }
